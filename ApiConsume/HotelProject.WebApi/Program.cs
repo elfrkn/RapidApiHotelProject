d4e6d8618154d91e5312b7c376962b820de726cf
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-06-19 16:04:56 +0200

    Booking pages

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -33,6 +33,10 @@ builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 builder.Services.AddScoped<IAboutDal, EfAboutDal>();
 builder.Services.AddScoped<IAboutService, AboutManager>();
 
+
+builder.Services.AddScoped<IBookingDal, EfBookingDal>();
+builder.Services.AddScoped<IBookingService, BookingManager>();
+
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 
