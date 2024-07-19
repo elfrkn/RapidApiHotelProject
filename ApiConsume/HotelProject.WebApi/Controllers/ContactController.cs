b5d6c1fed3ed6ec864ba316bee269548faf0cd14
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-07-19 16:11:46 +0200

    Message,Mail,ApiFile

--- ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
@@ -17,11 +17,30 @@ namespace HotelProject.WebApi.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddCoontact(Contact contact)
+        public IActionResult AddContact(Contact contact)
         {
             contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
             _contactService.TInsert(contact);
             return Ok();
         }
+
+        [HttpGet]
+        public IActionResult InboxListContact()
+        {
+            var values = _contactService.TGetList();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSendMessage(int id)
+        {
+            var values = _contactService.TGetByID(id);
+            return Ok(values);
+        }
+        //[HttpGet("GetContactCount")]
+        //public IActionResult GetContactCount()
+        //{
+        //    return Ok(_contactService.TGetContactCount());
+        //}
+
     }
 }
